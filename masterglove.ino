4ec03a34088bb9b2c0a7cee8e74d8365b143a488
kratenko <kratenko@garstig.org>
2018-05-17 14:30:10 +0200

    first menu

--- masterglove.ino
+++ masterglove.ino
@@ -35,6 +35,15 @@ boolean f1, f2, f3, f4;
 
 GloveButton b1(PIN_F1), b2(PIN_F2), b3(PIN_F3), b4(PIN_F4);
 
+uint8_t m_state = 0;
+uint8_t m_state_max = 2;
+uint8_t m_pos = 0;
+uint8_t m_bright = 3;
+uint8_t m_bright_max = 5;
+uint8_t m_mode = 0;
+uint8_t m_mode_max = 8;
+boolean m_print = true;
+
 void setup()
 {
   Wire.begin();
@@ -47,8 +56,15 @@ void setup()
   // Display.setTextXY(0,0);          //Set the cursor to Xth Page, Yth Column  
   // Display.putString("Hello World!"); //Print the String
 
-  Display.putString("ABCDEFGHIJ");
-  Display.putInvertedString("ABCDEFGHIJKlmnopqrstuvwxyz");
+  Display.setTextXY(1, 4);
+  Display.putInvertedString("Master");
+  Display.setTextXY(2, 4);
+  Display.putInvertedString("Control");
+  Display.setTextXY(3, 4);
+  Display.putInvertedString("Glove");
+  Display.setTextXY(5, 0);
+  Display.putString("initializing...");
+  delay(1500);
 
   //Display.putString("\x80\x80\x80\x81\x81\x81\x82\x82\x82\x83\x83\x83");
   //Display.putString("\x84\x84\x84\x85\x85\x85x\x86\x86\x86\x87\x87\x87oooxXOA");
@@ -81,18 +97,132 @@ void toggle_light() {
   }
 }
 
-uint8_t current_vest_mode = 0;
-char* vest_modes[] =  {"pulse", "other", "third"};
-uint8_t vest_modes_count = 3;
+void write_display() {
+  if ( !m_print ) {
+    return;
+  }
+  m_print = false;
+  if ( m_state == 0 ) {
+    // nothing
+  } else {
+    Display.setTextXY(0, 0);
+    if ( m_state == 1 ) {
+      // mode
+      Display.putInvertedString("Vest Modus");
+      Display.setTextXY(1, 0);
+      Display.putString("Mode ");
+      Display.putNumber(m_mode);
+      Display.putString("/");
+      Display.putNumber(m_mode_max);
+      Display.putString("  ");
+    } else if ( m_state == 2 ) {
+      // bright
+      Display.putInvertedString("Brightness");
+      Display.setTextXY(1, 0);
+      Display.putString("Level ");
+      Display.putNumber(m_bright);
+      Display.putString("/");
+      Display.putNumber(m_bright_max);
+      Display.putString("  ");
+    }
+  }
+}
 
-void write_vest_mode() {
-  Display.setTextXY(0, 0);
-  Display.putString("V: ");
-  Display.putNumber(current_vest_mode);
-  Display.putString("/");
-  Display.putNumber(vest_modes_count-1);
-  Display.putString(": ");
-  Display.putString(vest_modes[current_vest_mode]);
+void menu_leave() {
+  m_state = 0;
+  m_print = true;
+}
+
+void menu_menu() {
+  m_state += 1;
+  if ( m_state > m_state_max ) {
+    m_state = 0;
+  }
+  m_pos = 0;
+  Display.clearDisplay();
+  m_print = true;
+}
+
+void menu_next() {
+  // index finger down:
+  if ( m_state == 0) {
+    // none - send beat
+    send_vest(20);
+  }
+  if ( m_state == 1 ) {
+    // modus
+    m_mode +=1;
+    if ( m_mode > m_mode_max ) {
+      m_mode = 0;
+    }
+    send_mode();
+  }
+  if ( m_state == 2 ) {
+    // brightness
+    m_bright +=1;
+    if ( m_bright > m_bright_max ) {
+      m_bright = 0;
+    }
+    send_brightness();
+  }
+  m_print = true;
+}
+
+uint8_t m_mode_save = 255;
+// middle finger down
+void menu_prev() {
+  if ( m_state == 0 ) {
+    m_mode_save = m_mode;
+    m_mode = 7;
+    send_mode();
+  }
+  if ( m_state == 1 ) {
+    // modus
+    if ( m_mode == 0 ) {
+      m_mode = m_mode_max;
+    } else {
+    m_mode -=1;
+    }
+    send_mode();
+  }
+  if ( m_state == 2 ) {
+    // brightness
+    if ( m_bright == 0 ) {
+      m_bright = m_bright_max;
+    } else {
+      m_bright -=1;
+    }
+    send_brightness();
+  }
+  m_print = true;
+}
+
+// middle finger up
+void menu_prev_up() {
+  if ( m_state == 0 ) {
+    if ( m_mode_save != 255 ) {
+      m_mode = m_mode_save;
+      m_mode_save = 255;
+      send_mode();
+    }
+  }
+  m_print = true;
+}
+
+void send_vest(uint8_t cmd) {
+  Wire.beginTransmission(4);
+  Wire.write(cmd);
+  Wire.endTransmission();
+}
+
+void send_brightness() {
+  if ( m_bright > m_bright_max ) return;
+  send_vest(m_bright+200);
+}
+
+void send_mode() {
+  if ( m_mode > m_mode_max ) return;
+  send_vest(m_mode+100);
 }
 
 void loop() {
@@ -101,94 +231,45 @@ void loop() {
   b2.read();
   b3.read();
   b4.read();
+  // Index Finger (next):
   if ( b1.get_event() & GloveButton::PRESS ) {
-    Display.putChar(B_S);
-    col += 16;
-    Wire.beginTransmission(4);
-    Wire.write(col);
-    Wire.endTransmission();
+    // Display.putChar(B_S);
+    // col += 16;
+    menu_next();
   } else if ( b1.get_event() & GloveButton::HOLD ) {
-    Display.putChar(B_S_L);
+    // Display.putChar(B_S_L);
   } else if ( b1.get_event() & GloveButton::REPEAT ) {
-    Display.putChar(B_S_D);
+    // Display.putChar(B_S_D);
   } else if ( b1.get_event() & GloveButton::RELEASE ) {
-    Display.putChar(B_S_O);
+    // Display.putChar(B_S_O);
   }
   if ( b2.get_event() & GloveButton::PRESS ) {
-    Display.putChar(B_X);
-    col -= 16;
-    Wire.beginTransmission(4);
-    Wire.write(col);
-    Wire.endTransmission();
+    // Display.putChar(B_X);
+    // col -= 16;
+    menu_prev();
   } else if ( b2.get_event() & GloveButton::HOLD ) {
-    Display.putChar(B_X_L);
+    // Display.putChar(B_X_L);
   } else if ( b2.get_event() & GloveButton::REPEAT ) {
-    Display.putChar(B_X_D);
+    // Display.putChar(B_X_D);
   } else if ( b2.get_event() & GloveButton::RELEASE ) {
-    Display.putChar(B_X_O);
+    // Display.putChar(B_X_O);
+    menu_prev_up();
   }
   if ( b3.get_event() & GloveButton::PRESS ) {
-    Display.putString("3");
+    // Display.putString("3");
     toggle_light();
   } else if ( b3.get_event() & GloveButton::HOLD ) {
-    Display.putString("c");
+    // Display.putString("c");
   } else if ( b3.get_event() & GloveButton::REPEAT ) {
-    Display.putString("C");
+    // Display.putString("C");
   }
   if ( b4.get_event() & GloveButton::PRESS ) {
-    Display.putString("4");
+    // Display.putString("4");
+    menu_menu();
   } else if ( b4.get_event() & GloveButton::HOLD ) {
     Display.clearDisplay();
+    menu_leave();
   }
-
-  write_vest_mode();
-  return;
-  
-  col += 1;
-  Wire.beginTransmission(4);
-  Wire.write(col);
-  Wire.endTransmission();
-
-  return;
-
-  
-  
-  f1 = b1.get_down();
-  if ( f1 ) {
-    analogWrite(PIN_L1, 255);
-  } else {
-    analogWrite(PIN_L1, 0);
-  }
-  if ( f2 ) {
-    analogWrite(PIN_L2, 255);
-  } else {
-    analogWrite(PIN_L2, 0);
-  }
-  if ( f3 ) {
-    analogWrite(PIN_L3, 255);
-  } else {
-    analogWrite(PIN_L3, 0);
-  }
-  if ( f4 ) {
-    analogWrite(PIN_L4, 255);
-  } else {
-    analogWrite(PIN_L4, 0);
-  }
-  /*Display.setTextXY(0, 0);
-  Display.putString("Finger 4: ");
-  Display.putNumber(f4);
-  Display.setTextXY(1, 0);
-  Display.putString("Finger 3: ");
-  Display.putNumber(f3);
-  Display.setTextXY(2, 0);
-  Display.putString("Finger 2: ");
-  Display.putNumber(f2);*/
-  Display.setTextXY(3, 0);
-  Display.putString("Finger 1: ");
-  Display.putNumber(b1.get_cumulate());
-//  Display.setTextXY(7, 0);
-//  Display.putNumber(millis());
-  //delay(1);
-
+  write_display();
 }
 
